d9f647f1c39d5bf2fbb0d693ec59ea0313764fb4
AnderSolana <valearangu@hotmail.com>
2020-02-13 13:59:30 +0100

    backoofice

--- src/app/paginas/backoffice/backoffice.component.ts
+++ src/app/paginas/backoffice/backoffice.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackofficeComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+  }//constructor
 
   ngOnInit() {
-  }
 
-}
+  }//ngOnInit
+
+}//BackofficeComponent

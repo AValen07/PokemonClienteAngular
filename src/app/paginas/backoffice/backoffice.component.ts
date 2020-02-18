a4c39d9826dc0b20febb20fcef1df8e63821c52c
AnderSolana <valearangu@hotmail.com>
2020-02-18 10:34:06 +0100

    cambios pequeñicos

--- src/app/paginas/backoffice/backoffice.component.ts
+++ src/app/paginas/backoffice/backoffice.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/model/pokemon';
 import { PokemonService } from 'src/app/services/pokemon.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Habilidad } from 'src/app/model/habilidad';
+import { HabilidadService } from 'src/app/services/habilidad.service';
 
 
 @Component({
@@ -14,10 +16,19 @@ export class BackofficeComponent implements OnInit {
   pokemones:Array<Pokemon>;
   pokemonSeleccionado:Pokemon;
   formularioPokemon : FormGroup;
+  habilidadesFormulario:Array<any>;
+  habilidades:Array<Habilidad>;
+
+  // mensajes
+  mensaje: string;
+  showMensaje: boolean;
 
   constructor(private pokemonService:PokemonService,
-              private _builder : FormBuilder) { 
-
+              private _builder : FormBuilder,
+              private habilidadService:HabilidadService) { 
+    
+    this.habilidades = new Array<Habilidad>();
+    this.habilidadesFormulario = new Array<any>();
     this.pokemones = new Array<Pokemon>();
     this.pokemonSeleccionado= new Pokemon();
     console.trace('pokemonSeleccionado: %o', this.pokemonSeleccionado);
@@ -26,17 +37,21 @@ export class BackofficeComponent implements OnInit {
       nombre:['',[Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
       id:[0]
     });
+
+    this.mensaje='';
+    this.showMensaje=false;
     
   }//constructor
 
   ngOnInit() {
-    this.pokemonService.getAllPokemon().subscribe(
-      (lista)=>{
-        console.trace('hace cosas %o',lista);
-        this.pokemones=lista;
-        console.trace('hace cosas %o',this.pokemones);
+    this.cargarListado();
+    
+    this.habilidadService.getAllHabilidades().subscribe(el=>{
+      this.habilidades=el;
+      this.habilidadesFormulario = this.habilidades.map(el => {
+        return {name: el.nombre, value: el.nombre, checked: false}; 
       });
-      
+    });
   }//ngOnInit
 
   nuevo(){
@@ -60,7 +75,24 @@ export class BackofficeComponent implements OnInit {
     p.nombre=values.nombre;
 
     this.pokemonService.crear(p).subscribe(()=>{
-      console.debug('todo va bien');
+      this.mensaje='Se ha creado el pokemon correctamente';
+      this.showMensaje=true;
+      this.cargarListado();
+      this.nuevo();
+    },
+    (error)=>{
+      if(error.status==409){
+        console.debug('El pokemon introducido ya existe. %o', error);
+        this.mensaje='El pokemon introducido ya existe. Intentelo de nuevo.';
+        this.showMensaje=true;
+        this.nuevo();
+      } else{
+        console.debug('Algo ha ido bastante mal. %o', error);
+        this.mensaje='Error desconocido. Intentelo de nuevo.';
+        this.showMensaje=true;
+        this.nuevo();
+      }
+      
     });
   }
   modificar(values:any){
@@ -69,12 +101,33 @@ export class BackofficeComponent implements OnInit {
     p=new Pokemon();
     p.nombre=values.nombre;
     p.id=values.id;
-    this.pokemonService.modificar(values.id,p).subscribe();
+    this.pokemonService.modificar(values.id,p).subscribe(()=>{
+      this.mensaje='Se ha modificado el pokemon correctamente';   
+      this.showMensaje=true;
+      this.cargarListado();
+      this.nuevo();   
+    },
+    error=>{
+      if(error.status==409){
+        console.debug('El pokemon introducido ya existe. %o', error);
+        this.mensaje='El pokemon introducido ya existe. Intentelo de nuevo.';        
+      } else{
+        console.debug('Algo ha ido bastante mal. %o', error);
+        this.mensaje='Error desconocido. Intentelo de nuevo.';        
+      }      
+      this.showMensaje=true;
+      this.cargarListado();
+      this.nuevo();
+    });
   }
 
   eliminar(id:number){
     if(confirm('Estas seguro?')){
-      this.pokemonService.eliminar(id).subscribe();
+      this.pokemonService.eliminar(id).subscribe(()=>{
+      this.mensaje='Se ha eliminado el pokemon correctamente';
+      this.showMensaje=true;
+      this.cargarListado();
+      });
     }
   }
   seleccionarPokemon(pokemon:Pokemon){
@@ -83,4 +136,12 @@ export class BackofficeComponent implements OnInit {
     this.formularioPokemon.controls.id.setValue(this.pokemonSeleccionado.id);
   }
 
+  cargarListado(){
+    this.pokemonService.getAllPokemon().subscribe(
+      (lista)=>{
+        console.trace('hace cosas %o',lista);
+        this.pokemones=lista;
+        console.trace('hace cosas %o',this.pokemones);
+      });
+  }
 }//BackofficeComponent
